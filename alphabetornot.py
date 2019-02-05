81560639c1ac2f13e0f916d521ef996670fd3a47
giri711dharan <37236829+giri711dharan@users.noreply.github.com>
2019-02-05 14:40:42 +0530

    Create alphabetornot.py

---
+++ alphabetornot.py
@@ -0,0 +1,9 @@
+ch=raw_input()
+if ch == '0':
+   exit();
+else:
+    if((ch>='a' and ch<='z') or (ch>='A' and ch<='Z')):
+      print(ch, "is a alphabet");
+    else:
+      print(ch, "is not alphabet");
+      
