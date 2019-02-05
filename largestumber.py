bd99e0306c1d25692fd2177499e5a6fc2a6a9fb4
giri711dharan <37236829+giri711dharan@users.noreply.github.com>
2019-02-05 15:09:45 +0530

    Create largestumber.py

---
+++ largestumber.py
@@ -0,0 +1,11 @@
+a=raw_input()
+b=raw_input()
+c=raw_input()
+if((a>b)and(a>c)):
+ print("a is greater")
+elif((b>a)and(b>c)):
+  print("b is greater ")
+elif((a<c)and(b<c)):
+  print("c is greater ")
+else:
+  print("invalid")  
