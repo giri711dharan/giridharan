cb716a6f9b4fff91157d126229e906dfe4d5806b
giri711dharan <37236829+giri711dharan@users.noreply.github.com>
2019-02-05 12:30:32 +0530

    Create vovels.py

---
+++ vovels.py
@@ -0,0 +1,6 @@
+v=raw_input("enter the letter:")
+if(v=='a'or v=='A'or v=='e'or v=='E' or v=='i'or v=='I'or v=='o'or v=='O'or v=='u'or v=='U'):
+  print('the entred letter is vowel')
+else:
+  print('the entred letter is consonant')
+ 
